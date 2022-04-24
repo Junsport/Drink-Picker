3845358c07fb195f9e1930ce80688a663784887d
sergio flores <junie33@gmail.com>
2022-04-24 15:57:07 -0700

    added ingredients to DOM

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Getting error:  error TypeError: Cannot read properties of undefined (reading 'strIngredient7') when trying to push ingredients to DOM?????
+// Ingredients pushed to DOM but there is an extra comma at the beginning for unk reason
 
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
 document.querySelector('button').addEventListener('click', getDrink)
@@ -14,16 +14,15 @@ function getDrink(){
       let arr = []
       for (let i = 0; i < 15; i++){
         if(data.drinks[i][`strIngredient${i}`] !== null){
+          let listItems = data.drinks[i][`strIngredient${[i]}`]
+          arr.push(listItems)
           console.log(data.drinks[i][`strIngredient${[i]}`])
         }
-          arr.push([`strIngredient${[i]}`])
-      }
-
-      document.querySelector('h2').innerText = data.drinks[0].strDrink
-      document.querySelector('img').src = data.drinks[0].strDrinkThumb
-      document.querySelector('.instructions').innerText = data.drinks[0].strInstructions
-      document.querySelector(".ingredients").innerHTML = arr
-
+          document.querySelector('h2').innerText = data.drinks[0].strDrink
+          document.querySelector('img').src = data.drinks[0].strDrinkThumb
+          document.querySelector('.instructions').innerText = data.drinks[0].strInstructions
+          document.querySelector(".ingredients").innerText = arr.join(", ")
+        }
     })
     .catch(err => {
         console.log(`error ${err}`)
