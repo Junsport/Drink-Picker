85b88aeb692a2ec7d1bbe328064be20df9134b25
sergio flores <junie33@gmail.com>
2022-04-24 15:04:35 -0700

    Add ingredients to DOM unsuccessful

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Getting error:  error TypeError: Cannot read properties of undefined (reading 'strIngredient7') when trying to push ingredients to DOM?????
+
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
 document.querySelector('button').addEventListener('click', getDrink)
 
@@ -8,10 +10,19 @@ function getDrink(){
     .then(res => res.json()) // parse response as JSON
     .then(data => {
       console.log(data.drinks)
+
+      let arr = []
+      for (let i = 0; i < 15; i++){
+        if(data.drinks[i][`strIngredient${i}`] !== null){
+          console.log(data.drinks[i][`strIngredient${[i]}`])
+        }
+          arr.push([`strIngredient${[i]}`])
+      }
+
       document.querySelector('h2').innerText = data.drinks[0].strDrink
       document.querySelector('img').src = data.drinks[0].strDrinkThumb
       document.querySelector('.instructions').innerText = data.drinks[0].strInstructions
-      document.querySelector(".ingredients").innerText = data.drinks[0].strIngredient1
+      document.querySelector(".ingredients").innerHTML = arr
 
     })
     .catch(err => {
@@ -19,4 +30,4 @@ function getDrink(){
     });
 
 }
-
+ 
